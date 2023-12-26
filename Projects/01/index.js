0975c37f9989b37ffc3d4b935a4dcac0da1d6324
hgafrony@gmail.com <you@example.com>
2023-12-26 16:27:44 +0300

    init

---
+++ Projects/01/index.js
@@ -0,0 +1,31 @@
+// Funcions Style
+
+
+// Anonymous Function , function without name
+
+// function readLine(line, time) {
+
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log('readLine : ', arguments[i]);
+//     }
+// }
+
+
+//  Arrow Function (with curly braces)
+// const readBook =  (pageCount, BookType)=> {
+//     console.log(
+//         ' pageCount : ', pageCount, '\n',
+//         'BookType : ', BookType,
+//     )
+// }
+
+
+// Arrow Function (without curly braces) 
+// const readBook = (pageCount, BookType) => console.log(
+//     ' pageCount : ', pageCount, '\n',
+//     'BookType : ', BookType,)
+
+
+// Arrow Function for Addition
+// const add_number = (num1, num2, num3) => num1 + num2 + num3;
+// console.log(add_number(1, 2, 3));
